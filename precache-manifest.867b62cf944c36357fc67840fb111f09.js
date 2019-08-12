5278bfc8043cb71e61642e2a11f2ce07a8dd0e07
poorva-1 <poorvamohoni@gmail.com>
2019-08-12 15:03:22 +0530

    Updates

---
+++ precache-manifest.867b62cf944c36357fc67840fb111f09.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd2c6dd0ff21a75626bb8a4a50371af",
+    "url": "/poorva-1/react_project1.git/index.html"
+  },
+  {
+    "revision": "5cce429cfb28c7659f79",
+    "url": "/poorva-1/react_project1.git/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "49a0339b119899bb1a77",
+    "url": "/poorva-1/react_project1.git/static/css/main.f525f5e8.chunk.css"
+  },
+  {
+    "revision": "5cce429cfb28c7659f79",
+    "url": "/poorva-1/react_project1.git/static/js/2.b333596d.chunk.js"
+  },
+  {
+    "revision": "49a0339b119899bb1a77",
+    "url": "/poorva-1/react_project1.git/static/js/main.43e1bc16.chunk.js"
+  },
+  {
+    "revision": "ed5757b22477a5b0e0de",
+    "url": "/poorva-1/react_project1.git/static/js/runtime~main.f0a2f34d.js"
+  },
+  {
+    "revision": "b2110b2139be7265c389bcb1c0736da3",
+    "url": "/poorva-1/react_project1.git/static/media/application_development.b2110b21.png"
+  },
+  {
+    "revision": "8e515c26d5f2d1841b123811005a181c",
+    "url": "/poorva-1/react_project1.git/static/media/digital.8e515c26.jpg"
+  },
+  {
+    "revision": "9f7f62750b9a2b46ede3ba0a4699fb31",
+    "url": "/poorva-1/react_project1.git/static/media/logo.9f7f6275.png"
+  },
+  {
+    "revision": "d67c1cca0889a9b78d1c549733219b2a",
+    "url": "/poorva-1/react_project1.git/static/media/w1.d67c1cca.jpg"
+  },
+  {
+    "revision": "f29ed408b4fe986f6c279fc7cfca230b",
+    "url": "/poorva-1/react_project1.git/static/media/w2.f29ed408.jpg"
+  },
+  {
+    "revision": "330cf43224dd89ce58640bf250d10904",
+    "url": "/poorva-1/react_project1.git/static/media/w3.330cf432.jpg"
+  },
+  {
+    "revision": "84825c5704118d4e22dfc47b9cea5733",
+    "url": "/poorva-1/react_project1.git/static/media/w4.84825c57.jpg"
+  },
+  {
+    "revision": "c5989fa8f5f1c77d7894278759f05071",
+    "url": "/poorva-1/react_project1.git/static/media/w5.c5989fa8.jpg"
+  },
+  {
+    "revision": "4af81a156398427bb723613c7f2dcd1c",
+    "url": "/poorva-1/react_project1.git/static/media/w6.4af81a15.png"
+  },
+  {
+    "revision": "7d516862f797757862805fd132ca1e6b",
+    "url": "/poorva-1/react_project1.git/static/media/w7.7d516862.jpg"
+  },
+  {
+    "revision": "b90021d6d0f86bec7dd4d91e3c12930e",
+    "url": "/poorva-1/react_project1.git/static/media/w8.b90021d6.png"
+  },
+  {
+    "revision": "264d8fb4ff3d916de76d2c03046ad500",
+    "url": "/poorva-1/react_project1.git/static/media/w9.264d8fb4.png"
+  },
+  {
+    "revision": "c1cf094ab09ec79959dd305b3d4a7beb",
+    "url": "/poorva-1/react_project1.git/static/media/web_development.c1cf094a.png"
+  },
+  {
+    "revision": "bc2f64ff230cbf4b8f6093c574985022",
+    "url": "/poorva-1/react_project1.git/static/media/website-development.bc2f64ff.jpg"
+  }
+]);
